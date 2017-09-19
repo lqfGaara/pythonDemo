29c83b418db2a2d99926f54ce826c64810bbf921
lqf <luoqingfeng5918@163.com>
2017-09-19 10:09:38 +0800

    第一次提交

---
+++ test1.py
@@ -0,0 +1,12 @@
+class Test:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def test(self):
+        print(self.age)
+        print(self.name)
+
+
+T = Test("stone", 12)
+T.test()
